c884dfe5038fd46041fe84af28231bec8c5885b4
enzojbnss <enzojbnss@gmail.com>
2022-02-23 09:30:45 -0300

    implemetação de bannes

---
+++ resources/views/vendor/grapesjs/templates/simple_banner.blade.php
@@ -0,0 +1,6 @@
+
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/home-bannerpngyesh_1633019136785495204.png" alt="Los Angeles" class="d-block" style="width:100%">
+  </div>
+</div>
