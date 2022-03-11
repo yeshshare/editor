16fe16e21d8ef27d08653bae03d17c5f06a49ad1
enzojbnss <enzojbnss@gmail.com>
2022-03-11 17:11:13 -0300

    ultimos ajustes

--- public/js/lp.js
+++ public/js/lp.js
@@ -15,26 +15,61 @@ function init() {
 
     load_parameters();
     console.log(logado == 'n');
-    try {
-        show_element(("menu_registerd"), (logado == 's'));
-        show_element(("menu_unregisterd"), (logado == 'n'));
-    } catch (error) {
-        console.log(error);
-    };
-    try {
-        var login = document.getElementById("login");
-        login.style.display = (accessType == 1 || accessType == 2) ? "block" : "none";
-        let lista = document.querySelectorAll("section");
-        [].forEach.call(lista, (e) => {
-            if (e.id != login.id) {
-                console.log(e.id)
-                e.style.display = (accessType == 0 || accessType == 2) ? "block" : "none";
+    if (element_exists("menu")) {
+        try {
+            show_element(("menu_registerd"), (logado == 's'));
+            show_element(("menu_unregisterd"), (logado == 'n'));
+        } catch (error) {
+            console.log(error);
+        };
+    }
+    if (element_exists("login")) {
+        try {
+            var login = document.getElementById("login");
+            login.style.display = (accessType == 1 || accessType == 2) ? "block" : "none";
+            let lista = document.querySelectorAll("section");
+            [].forEach.call(lista, (e) => {
+                if (e.id != login.id) {
+                    console.log(e.id)
+                    e.style.display = (accessType == 0 || accessType == 2) ? "block" : "none";
+                }
+            });
+        } catch (error) {
+
+        };
+    }
+    if (element_exists("cardScheduleSpeakers")) {
+        var cardScheduleSpeakersMenu = get_element("cardScheduleSpeakersMenu");
+        cardScheduleSpeakersMenuLista = [
+            ["24/04", "home"],
+            ["25/04", "profile"],
+            ["26/04", "contact"]
+        ];
+
+        [].forEach.call(cardScheduleSpeakersMenuLista, (e, index) => {
+            let button = getButton(e[0], e[1], index);
+            let linha = getLI(button);
+            try {
+                console.log(linha);
+                cardScheduleSpeakersMenu.append(linha);
+            } catch (error) {
+
             }
         });
-    } catch (error) {
 
-    };
 
+
+        let images = [];
+        for (let contador = 0; contador <= 8; contador++) {
+            images.push("https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png");
+        }
+
+        var palestrates = get_element("palestrates");
+        var imagePalestrantes = getDiVImgPalestrantes(images);
+        var buttonEntrar = getButtonEntra();
+        palestrates.append(imagePalestrantes);
+        palestrates.append(buttonEntrar);
+    }
 
 };
 
@@ -122,4 +157,85 @@ function openNav(x) {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
     document.getElementById("btn_H").style.display = "block";
+}
+
+
+
+
+function getButton(label, id, active) {
+    let button = document.createElement("button");
+    let target = "#" + id;
+    let controls = id;
+    id = id + "-tab";
+    button.setAttribute("class", (active == 0) ? "btn btn-primary button_tab active" : "btn btn-primary button_tab");
+    button.setAttribute("id", id);
+    button.setAttribute("data-bs-toggle", "tab");
+    button.setAttribute("data-bs-target", target);
+    button.setAttribute("type", "button");
+    button.setAttribute("role", "tab")
+    button.setAttribute("aria-controls", controls);
+    button.setAttribute("aria-selected", "true");
+    button.innerHTML = label;
+    return button;
+}
+
+function getLI(button) {
+    let linha = document.createElement("li");
+    linha.setAttribute("class", "nav-item");
+    linha.setAttribute("role", "presentation");
+    linha.append(button);
+    if (typeof parent.gjsEditor == "undefined") {
+        return linha;
+    } else {
+        let ul = document.createElement("ul");
+        ul.append(linha);
+        return ul.innerHTML;
+    }
+}
+
+function getDiVImgPalestrantes(images) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row speaker_icons");
+    [].forEach.call(images, (url) => {
+        let img = document.createElement("img");
+        img.setAttribute("class", "card-img-top");
+        img.setAttribute("alt", "Card image");
+        img.setAttribute("style", "width: 100%;");
+        img.setAttribute("src", url);
+        let col = document.createElement("div");
+        col.setAttribute("class", "col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha");
+        col.append(img);
+        row.append(col);
+    });
+    let quadroImage = document.createElement("div");
+    quadroImage.setAttribute("class", "col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11");
+    quadroImage.append(row);
+    if (typeof parent.gjsEditor == "undefined") {
+        return quadroImage;
+    } else {
+        let div = document.createElement("div");
+        div.append(quadroImage);
+        return div.innerHTML;
+    }
+}
+
+
+function getButtonEntra(label, id, active) {
+    let button = document.createElement("button");
+    let target = "#" + id;
+    let controls = id;
+    id = id + "-tab";
+    button.setAttribute("class", "btn btn-primary btn_full");
+    button.setAttribute("href", "#");
+    button.innerHTML = "Entrar;";
+    let quadroButton = document.createElement("div");
+    quadroButton.setAttribute("class", "col-12 col-sm-12 col-md-12 col-lg-1 col-xl-1 center_Itens_end");
+    quadroButton.append(button)
+    if (typeof parent.gjsEditor == "undefined") {
+        return quadroButton;
+    } else {
+        let div = document.createElement("div");
+        div.append(quadroButton);
+        return div.innerHTML;
+    }
 }
--- resources/views/vendor/grapesjs/templates/card_agenda_palestrantes.blade.php
+++ resources/views/vendor/grapesjs/templates/card_agenda_palestrantes.blade.php
@@ -1,83 +1,46 @@
-  <ul class="nav justify-content-center" id="myTab" role="tablist">
-    <li class="nav-item" role="presentation">
-      <button class="btn btn-primary button_tab active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">24/04</button>
-    </li>
-    <li class="nav-item" role="presentation">
-      <button class="btn btn-primary button_tab" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">25/04</button>
-    </li>
-    <li class="nav-item" role="presentation">
-      <button class="btn btn-primary button_tab" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">26/04</button>
-    </li>
-  </ul>
-  <div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-        <div class="container">
-            <div class="row center_Itens">
-                <div class="-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 space_botton">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2 ">
-                                <img class="card-img-top event_image" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/banner-cards-eventos-04-v01-11ago21pngyesh_16330192201034196461.png" alt="Card image" style="width: 100%;" />
-                            </div> 	
-                            <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
-                                <div class="card-body itens_agenda">
-                                    <h4 class="card-title">Evento de desmonstração</h4>
-                                    <div class="row ">
-                                        <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
-                                            15H30
+<section id="cardScheduleSpeakers">
+    <ul class="nav justify-content-center" id="cardScheduleSpeakersMenu" role="tablist">
+        
+    </ul>
+    <div class="tab-content" id="cardScheduleSpeakersContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <div class="container">
+                <div class="row center_Itens">
+                    <div class="-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 space_botton">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                                    <img
+                                        class="card-img-top event_image"
+                                        src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/banner-cards-eventos-04-v01-11ago21pngyesh_16330192201034196461.png"
+                                        alt="Card image"
+                                        style="width: 100%;"
+                                    />
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
+                                    <div class="card-body itens_agenda">
+                                        <h4 class="card-title">Evento de desmonstração</h4>
+                                        <div class="row">
+                                            <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                                                15H30
+                                            </div>
+                                            <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
+                                                <p class="card-text">Este é um evento demostrativo do funcinamento da plataforma</p>
+                                            </div>
                                         </div>
-                                        <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
-                                            <p class="card-text">Este é um evento demostrativo do funcinamento da plataforma</p>
+
+                                        <div class="row" id="palestrates">
+                                            
                                         </div>
-                                    </div>
-                                    
-                                    <div class="row">
-                                        <div class="col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11">
-                                            <div class="row .speaker_icons">
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>
-                                                <div class="col-4 col-sm-4 col-md-2 col-lg-1 col-xl-1 link_social_linha">
-                                                    <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:80px">
-                                                </div>                                                                                          
-                                            </div>    
-                                        </div>
-                                        
-                                        <div class="col-12 col-sm-12 col-md-12 col-lg-1 col-xl-1 center_Itens_end">
-                                            <a href="#" class="btn btn-primary btn_full">Entrar</a>
-                                        </div>                                        
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>                   
+                </div>
             </div>
-        </div>  
+        </div>
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"></div>
+        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab"></div>
     </div>
-    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-
-    </div>
-    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-
-    </div>
-  </div>
-
+</section>
