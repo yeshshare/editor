bad031bf4db7759b3fd9652002faf275d9554a17
enzojbnss <enzojbnss@gmail.com>
2022-03-25 12:06:53 -0300

    ajuste template e plugins

--- config/grapesjs.php
+++ config/grapesjs.php
@@ -15,7 +15,8 @@ return [
     'scripts' => ["https://cdnjs.cloudflare.com/ajax/libs/grapesjs/0.12.17/grapes.min.js",
         "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
         "../../js/lp.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"
-       
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js",
+        "../../js/grapejs-nav-bar/grapesjs-navbar.min.js",
+        "../../js/grapejs/plugins.js"       
     ]
 ];
---
+++ resources/views/vendor/old/basic_card.blade.php
@@ -0,0 +1,128 @@
+<div class="container">
+    <div class="row center_Itens">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center space_botton">
+            <h1 class="text-white">           
+            Palestrantes 
+            </h1>
+        </div>
+    </div>                
+</div>
+<div class="container">
+    <div class="row center_Itens">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 space_botton">
+            <div class="card" >
+                <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:100%">
+                <div class="card-body text-center">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example</p>
+                    <div class="row">
+                    <div class="col link_social_linha">
+                            <i class="fa-brands fa-facebook-square link_social"></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-instagram link_social "></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-twitter-square link_social "></i>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>    
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 space_botton">
+            <div class="card" >
+                <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:100%">
+                <div class="card-body text-center">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example</p>
+                    <div class="row">
+                    <div class="col link_social_linha">
+                            <i class="fa-brands fa-facebook-square link_social"></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-instagram link_social "></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-twitter-square link_social "></i>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>    
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 space_botton">
+            <div class="card" >
+                <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:100%">
+                <div class="card-body text-center">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example</p>
+                    <div class="row">
+                    <div class="col link_social_linha">
+                            <i class="fa-brands fa-facebook-square link_social"></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-instagram link_social "></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-twitter-square link_social "></i>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>    
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 space_botton">
+            <div class="card" >
+                <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:100%">
+                <div class="card-body text-center">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example</p>
+                    <div class="row">
+                    <div class="col link_social_linha">
+                            <i class="fa-brands fa-facebook-square link_social"></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-instagram link_social "></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-twitter-square link_social "></i>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>    
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 space_botton">
+            <div class="card" >
+                <img class="card-img-top" src="https://yeshmeet.yeshmeet.com.br/storage//YESH/landingpage/imagens/plataforma-yesh-layout-de-teste-foto-de-palestrante-1pngyesh_1616077381177556524.png" alt="Card image" style="width:100%">
+                <div class="card-body text-center">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example</p>
+                    <div class="row">
+                    <div class="col link_social_linha">
+                            <i class="fa-brands fa-facebook-square link_social"></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-instagram link_social "></i>
+                        </div>
+                        <div class="col link_social_linha">
+                            <i class="fa-brands fa-twitter-square link_social "></i>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>    
+                    </div>  
+                </div>
+            </div>
+        </div>     
+    </div>
+</div>   
